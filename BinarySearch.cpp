54094af1fca41df67ca049e49b16ece41a36b741
faisal <fmohd743@gmail.com>
2024-06-10 14:55:34 +0530

    binary search

---
+++ BinarySearch.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int start=0;
+    int end=5;
+    int arr[5]={12,13,14,15,16};
+    int key=15;
+    
+while(start<=end){  
+    int mid_Ele=(start+end)/2;
+    if(arr[mid_Ele]==key){
+        cout<<"Got it:"<<mid_Ele;
+        break;
+    }
+   else  if(arr[mid_Ele]<key){
+            start=mid_Ele+1;
+            cout<<"loop end in elseif"<<endl;
+        }
+        
+        else{
+                end=mid_Ele-1;       
+        }
+}
+return 0;
+    }
+    
