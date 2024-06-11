3c5c5327b8e964b6cff136a5d7b970c2fe7a14c2
faisal <fmohd743@gmail.com>
2024-06-11 16:03:59 +0530

    missing kth number

---
+++ missingkthNumber.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class missingNumber{
+    public:
+    int missingVal(int arr[],int k){
+        int missingCount=0;
+        int currentNumber=1;
+        int arrIndex=0;
+        
+    }
+};
+int main(){
+    int arr[6]={1,2,4,5,6,7};
+    int k=3;
+    int n=6;
+    missingNumber mn;
+
+}
